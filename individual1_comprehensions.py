2a59e807c5014d1535719fb4d89eeb6127162f81
13r4lera <korobkaval@icloud.com>
2026-01-24 12:19:09 +0300

    Update individual1_comprehensions.py

--- individual1_comprehensions.py
+++ individual1_comprehensions.py
@@ -11,7 +11,8 @@ if __name__ == '__main__':
         print("Неверный размер кортежа", file=sys.stderr)
         sys.exit(1)
 
-    s = sum([x for x in A if 2 < x < 20 and x % 8 == 0])
-    l = len([x for x in A if 2 < x < 20 and x % 8 == 0])
+    my_list = [x for x in A if 2 < x < 20 and x % 8 == 0]
+    s = sum(my_list)
+    l = len(my_list)
 
     print(f"Сумма: {s}, количество: {l}")
